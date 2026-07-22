b567b3b9f1b71695e9a6148b1e721b7450cf7f96
Wilbert den Hertog <wmdenhertog@gmail.com>
2026-07-22 15:02:20 +0200

    Fallback git_version

---
+++ include/git_version.h
@@ -0,0 +1,8 @@
+// FALLBACK HEADER
+// This file exists solely to satisfy IDE indexers immediately after cloning.
+// During build, CMake will generate a real version of this file in the
+// build directory, which will shadow this one.
+
+#pragma once
+#define GIT_SHA "unknown"
+#define GIT_DIRTY 1
